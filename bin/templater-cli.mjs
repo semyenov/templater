f5be31bc39ac395361aef168234571d63e9465f4
Alexander Semyenov <semyenov@hotmail.com>
2023-07-19 17:20:52 +0300

    good

--- bin/templater-cli.mjs
+++ bin/templater-cli.mjs
@@ -1,8 +1,11 @@
-import color from 'picocolors'
-import { consola } from 'consola'
+#!/usr/bin/env node
+
 import * as p from '@clack/prompts'
 
 import { run } from '../dist/esm/index.mjs'
+
+import { consola } from 'consola'
+import color from 'picocolors'
 
 const logger = consola.withDefaults({
   tag: 'templater',
@@ -10,5 +13,5 @@ const logger = consola.withDefaults({
 
 p.intro(color.bgCyan(color.black(' Project generator ')))
 run()
+  .then(() => p.outro(color.bgGreen(color.black(' Done '))))
   .catch(logger.error)
-  .finally(() => p.outro(color.bgGreen(color.black(' Done '))))
