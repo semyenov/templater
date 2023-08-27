6ddecf79683d0b8e28fc9a64952d4b6f3560cffe
Alexander Semyenov <semyenov@hotmail.com>
2023-08-28 02:36:30 +0300

    upd

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,27 @@
+import { glob as glob_1 } from 'glob'
 import consola from 'consola'
 
-import * as lib from './lib'
+import { PromptManager, TEMPLATES_ROOT, generate, loadConfig } from './lib'
 
 const logger = consola.withDefaults({ tag: 'templater' })
-lib.run().catch(logger.error)
+run().catch(logger.error)
+
+async function run() {
+  PromptManager.intro()
+
+  const templates = await glob_1(`${TEMPLATES_ROOT}/**/*.yml`, {})
+  const template = await PromptManager.promptTemplate(templates)
+
+  const config = loadConfig(template)
+  const variables = await PromptManager.promptVariables(config.variables)
+
+  for (const file of config.files) {
+    await generate({
+      ...file,
+      template,
+      variables,
+    })
+  }
+
+  PromptManager.done()
+}
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,26 +1,4 @@
-import * as glob from 'glob'
-
-import { TEMPLATES_ROOT } from './constants'
-import { PromptManager } from './prompt'
-import { loadConfig } from './config'
-import { generate } from './generate'
-
-export async function run() {
-  PromptManager.intro()
-
-  const templates = await glob.glob(`${TEMPLATES_ROOT}/**/*.yml`, {})
-  const template = await PromptManager.promptTemplate(templates)
-
-  const config = loadConfig(template)
-  const variables = await PromptManager.promptVariables(config.variables)
-
-  for (const file of config.files) {
-    await generate({
-      ...file,
-      template,
-      variables,
-    })
-  }
-
-  PromptManager.done()
-}
+export { TEMPLATES_ROOT } from './constants'
+export { PromptManager } from './prompt'
+export { loadConfig } from './config'
+export { generate } from './generate'
