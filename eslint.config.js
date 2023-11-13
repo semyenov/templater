f4fa187e1840f2fec88e5655d88c9b8d3bfac69a
Alexander Semyenov <semyenov@hotmail.com>
2023-11-13 19:02:33 +0000

    .

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,34 @@
-import*as e from"@antfu/eslint-config";const r=e.antfu({rules:{"antfu/if-newline":["error"],"import/order":["error",{"newlines-between":"always",distinctGroup:!0,groups:["builtin","external","object","parent","internal","sibling","index","type"],pathGroups:[{pattern:"@/**",group:"internal",position:"after"},{pattern:"~/**",group:"internal",position:"after"}],alphabetize:{order:"asc",orderImportKind:"asc",caseInsensitive:!1}}]}});export{r as default};
+import * as e from '@antfu/eslint-config'
+
+const r = e.antfu({
+  rules: {
+    'antfu/if-newline': ['error'],
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        'distinctGroup': !0,
+        'groups': [
+          'builtin',
+          'external',
+          'object',
+          'parent',
+          'internal',
+          'sibling',
+          'index',
+          'type',
+        ],
+        'pathGroups': [
+          { pattern: '@/**', group: 'internal', position: 'after' },
+          { pattern: '~/**', group: 'internal', position: 'after' },
+        ],
+        'alphabetize': {
+          order: 'asc',
+          orderImportKind: 'asc',
+          caseInsensitive: !1,
+        },
+      },
+    ],
+  },
+})
+export { r as default }
